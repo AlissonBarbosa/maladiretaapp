d5021bc28f4b95e9ac5752aec7870ec2d44864b7
AlissonBarbosa <alissonnbarbosaa@gamil.com>
2020-07-22 13:32:13 -0300

    Version 0.0.1 - Changing the database

--- maladireta/settings.py
+++ maladireta/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os, environ, django_heroku
+import os, environ, django_heroku, dj_database_url
 
 root = environ.Path(__file__) - 3  # get root of the project
 env = environ.Env()
@@ -82,13 +82,14 @@ WSGI_APPLICATION = 'maladireta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
