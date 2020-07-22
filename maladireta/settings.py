9dde4bcec109d7068cdbf3cb73c4e5a5fff2057a
AlissonBarbosa <alissonnbarbosaa@gamil.com>
2020-07-22 13:36:55 -0300

    Version 0.0.1 - Changing the database

--- maladireta/settings.py
+++ maladireta/settings.py
@@ -89,7 +89,11 @@ WSGI_APPLICATION = 'maladireta.wsgi.application'
 #     }
 # }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
