d4329a8181199c31dc336098e0f80df1e5a7d711
Alisson Barbosa <alissonnbarbosaa@gmail.com>
2021-02-17 01:00:20 -0300

    Version 0.0.8 - Fixing reports templates

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-#from django.core.files.storage import FileSystemStorage
 from leadership.models import Leadership
 from employees.models import Employee
 from customers.models import Customer
@@ -11,6 +10,10 @@ from django.template.loader import render_to_string
 from weasyprint import HTML
 import tempfile
 from statistics.generate import Generate
+
+import io
+from django.http import FileResponse
+import pdfkit
 
 @login_required
 def handler404(request, exception):
@@ -46,73 +49,115 @@ def export_pdf(html_string, filename):
 
 def export_leadership_pdf(query): 
     html_string = render_to_string('reports/leadership_template.html', {'leaderships': query})
-    
-    html = HTML(string=html_string)
-    result = html.write_pdf()
+    options = {
+        'page-size': 'A4',
+        'encoding': "UTF-8",
+        'orientation': 'Landscape',
+        'margin-top': '1cm',
+        'margin-right': '1cm',
+        'margin-bottom': '1cm',
+        'margin-left': '1cm',
+        'footer-left': "Mala Direta - Romero Rodrigues",
+        'footer-font-size':'8',
+        'footer-right': '[page] de [topage]',
+        'footer-font-name': 'Dosis',
+    }
 
-    response = HttpResponse(content_type='application/pdf;')
-    response['Content-Disposition'] = 'inline; filename=Relatorio_Liderancas.pdf'
-    response['Content-Transfer-Encoding'] = 'binary'
-    with tempfile.NamedTemporaryFile(delete=True) as output:
-        output.write(result)
-        output.flush()
-        output = open(output.name, 'rb')
-        response.write(output.read())
+    output = pdfkit.from_string(html_string, False, options)
+    response = HttpResponse(output, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment;filename="Relatorio_Lideranças.pdf"'
 
     return response
 
+# def export_leadership_pdf(query):
+#     html_string = render_to_string('reports/leadership_template.html', {'leaderships': query})
+#     return export_pdf(html_string=html_string, filename="Relatorio Lideranças")
+
 def export_employee_pdf(query):
     html_string = render_to_string('reports/employee_template.html', {'employees': query})
-    return export_pdf(html_string=html_string, filename="Relatorio Funcionarios")
+    options = {
+        'page-size': 'A4',
+        'encoding': "UTF-8",
+        'orientation': 'Landscape',
+        'margin-top': '1cm',
+        'margin-right': '1cm',
+        'margin-bottom': '1cm',
+        'margin-left': '1cm',
+        'footer-left': "Mala Direta - Romero Rodrigues",
+        'footer-font-size':'8',
+        'footer-right': '[page] de [topage]',
+        'footer-font-name': 'Dosis',
+    }
+
+    output = pdfkit.from_string(html_string, False, options)
+    response = HttpResponse(output, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment;filename="Relatorio_Funcionários.pdf"'
+
+    return response
 
 def export_customer_pdf(query):
     html_string = render_to_string('reports/customer_template.html', {'customers': query})
-    
-    html = HTML(string=html_string)
-    result = html.write_pdf()
+    options = {
+        'page-size': 'A4',
+        'encoding': "UTF-8",
+        'orientation': 'Landscape',
+        'margin-top': '1cm',
+        'margin-right': '1cm',
+        'margin-bottom': '1cm',
+        'margin-left': '1cm',
+        'footer-left': "Mala Direta - Romero Rodrigues",
+        'footer-font-size':'8',
+        'footer-right': '[page] de [topage]',
+        'footer-font-name': 'Dosis',
+    }
 
-    response = HttpResponse(content_type='application/pdf;')
-    response['Content-Disposition'] = 'inline; filename=Relatorio_Clientes.pdf'
-    response['Content-Transfer-Encoding'] = 'binary'
-    with tempfile.NamedTemporaryFile(delete=True) as output:
-        output.write(result)
-        output.flush()
-        output = open(output.name, 'rb')
-        response.write(output.read())
+    output = pdfkit.from_string(html_string, False, options)
+    response = HttpResponse(output, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment;filename="Relatorio_Clientes.pdf"'
 
     return response
 
 def export_candidate_pdf(query):
     html_string = render_to_string('reports/candidate_template.html', {'candidates': query})
-    
-    html = HTML(string=html_string)
-    result = html.write_pdf()
+    options = {
+        'page-size': 'A4',
+        'encoding': "UTF-8",
+        'orientation': 'Landscape',
+        'margin-top': '1cm',
+        'margin-right': '1cm',
+        'margin-bottom': '1cm',
+        'margin-left': '1cm',
+        'footer-left': "Mala Direta - Romero Rodrigues",
+        'footer-font-size':'8',
+        'footer-right': '[page] de [topage]',
+        'footer-font-name': 'Dosis',
+    }
 
-    response = HttpResponse(content_type='application/pdf;')
-    response['Content-Disposition'] = 'inline; filename=Relatorio_Candidatos.pdf'
-    response['Content-Transfer-Encoding'] = 'binary'
-    with tempfile.NamedTemporaryFile(delete=True) as output:
-        output.write(result)
-        output.flush()
-        output = open(output.name, 'rb')
-        response.write(output.read())
+    output = pdfkit.from_string(html_string, False, options)
+    response = HttpResponse(output, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment;filename="Relatorio_Candidatos.pdf"'
 
     return response
 
 def export_authoritie_pdf(query):
     html_string = render_to_string('reports/authoritie_template.html', {'authorities': query})
-    
-    html = HTML(string=html_string)
-    result = html.write_pdf()
+    options = {
+        'page-size': 'A4',
+        'encoding': "UTF-8",
+        'orientation': 'Landscape',
+        'margin-top': '1cm',
+        'margin-right': '1cm',
+        'margin-bottom': '1cm',
+        'margin-left': '1cm',
+        'footer-left': "Mala Direta - Romero Rodrigues",
+        'footer-font-size':'8',
+        'footer-right': '[page] de [topage]',
+        'footer-font-name': 'Dosis',
+    }
 
-    response = HttpResponse(content_type='application/pdf;')
-    response['Content-Disposition'] = 'inline; filename=Relatorio_Autoridades.pdf'
-    response['Content-Transfer-Encoding'] = 'binary'
-    with tempfile.NamedTemporaryFile(delete=True) as output:
-        output.write(result)
-        output.flush()
-        output = open(output.name, 'rb')
-        response.write(output.read())
+    output = pdfkit.from_string(html_string, False, options)
+    response = HttpResponse(output, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment;filename="Relatorio_Autoridades.pdf"'
 
     return response
 
@@ -134,7 +179,7 @@ def generate_report(request):
         if query_filter:
             return export_customer_pdf(Customer.objects.search(query_filter))
         else:
-            return export_customer_pdf(Customer.objects.all())
+            return export_customer_pdf(Customer.objects.all()[:5000])
     elif origin == "Candidate":
         if query_filter:
             return export_candidate_pdf(Candidate.objects.search(query_filter))
